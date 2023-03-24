2459bc8c0ba18d663f6237a217706f87f887a2c5
tvoxrmktris <trib2017014@student.ctu.edu.vn>
2023-03-24 16:29:44 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,24 @@ import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [
+
+    {
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+
     {
         path: "/:pathMath(.*)*",
         name: "notfound",
         component: ()=> import("@/views/NotFound.vue"),
+    },
+    
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
     },
 ];
 
